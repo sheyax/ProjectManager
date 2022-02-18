4ac09b6b656db05548940b539d29349764806600
sheyisax <olusheyax@gmail.com>
2022-02-18 13:26:52 +0000

    new com

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Center from '../components/Center'
 import Sidebar from '../components/Sidebar'
-import styles from '../styles/Home.module.css'
+
 
 
 
